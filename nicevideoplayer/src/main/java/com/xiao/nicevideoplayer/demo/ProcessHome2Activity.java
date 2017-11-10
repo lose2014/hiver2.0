6d3979efd29150b39e7edc0dd1b33716fbedd700
lose2014 <396120192@qq.com>
2017-11-10 18:01:40 +0800

    视频

---
+++ nicevideoplayer/src/main/java/com/xiao/nicevideoplayer/demo/ProcessHome2Activity.java
@@ -0,0 +1,27 @@
+package com.xiao.nicevideoplayer.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xiao.nicevideoplayer.NiceVideoPlayerManager;
+import com.xiao.nicevideoplayer.R;
+
+public class ProcessHome2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_home2);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, new DemoProcessHomeKeyFragenment())
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (NiceVideoPlayerManager.instance().onBackPressd()) return;
+        super.onBackPressed();
+    }
+}
